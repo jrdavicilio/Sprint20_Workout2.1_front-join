74a608a7e8f021132d3286c360c0befa13806efd
David <jrdavicilio14@gmail.com>
2025-07-23 13:22:46 +0200

    PROYECTO FINAL

--- src/Home.jsx
+++ src/Home.jsx
@@ -1,7 +1,16 @@
-const Home = () => {
+import { Link } from "react-router-dom";
+
+const Home = ({data}) => {
   return (
     <>
-    
+      <h2>Lista de datos</h2>
+      <ul>
+        {data.map(item => (
+          <li key={item.id}>
+            <Link to={`/${item._id}`}>{item.title}</Link>
+          </li>
+        ))}
+      </ul>
     </>
   )
 };
